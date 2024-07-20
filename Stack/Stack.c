f0f836fa0bedcb68cba7188521c90e3a7d2f8d6e
Alimedhat000 <142531761+Alimedhat000@users.noreply.github.com>
2024-07-20 05:12:30 +0300

    added Sort_Stack

--- Stack/Stack.c
+++ Stack/Stack.c
@@ -2,19 +2,34 @@
 
 int main()
 {
-    push(5);
-    push(4);
-    push(3);
-    push(2);
-    push(1);
-    printStack();
-    pop();
-    pop();
-    pop();
-    pop();
-    pop();
-    pop();
-    printStack();
+    Stack *S = createStack();
+
+    push(9, S);
+    push(7, S);
+    push(5, S);
+    push(2, S);
+    push(2, S);
+    push(1, S);
+    push(3, S);
+
+    printf("stack before sorting :\n");
+    printStack(S);
+
+    printf("stack after sorting :\n");
+    sortStack(S);
+    printStack(S);
+}
+
+Stack *createStack()
+{
+    Stack *newStack = malloc(sizeof(node));
+    if (newStack != NULL)
+    {
+        newStack->pTop = NULL;
+
+        return newStack;
+    }
+    return NULL;
 }
 
 node *createNode(int data)
@@ -25,39 +40,36 @@ node *createNode(int data)
     {
         pNode->data = data;
         pNode->pNext = NULL;
-        size++;
     }
     return pNode;
 }
 
-void push(int data)
+void push(int data, Stack *S)
 {
     node *pNode = createNode(data);
     if (pNode == NULL)
     {
         return;
     }
-    pNode->pNext = pTop;
-    pTop = pNode;
-    size++;
+    pNode->pNext = S->pTop;
+    S->pTop = pNode;
     return;
 }
 
-int pop()
+int pop(Stack *S)
 {
-    if (pTop == NULL)
+    if (S->pTop == NULL)
     {
         printf("Stack is empty.\n");
-        return -1;
+        return -128;
     }
 
-    int data = pTop->data;
-    node *ptemp = pTop;
-    pTop = pTop->pNext;
+    int data = S->pTop->data;
+    node *ptemp = S->pTop;
+    S->pTop = S->pTop->pNext;
     free(ptemp);
-    size--;
 
-    if (pTop == NULL)
+    if (S->pTop == NULL)
     {
         printf("Poped all elements, the stack is now empty.\n");
     }
@@ -65,13 +77,48 @@ int pop()
     return data;
 }
 
-void printStack()
+void printStack(Stack *S)
 {
-    node *pCur = pTop;
-    for (int i = 0; i < size; i++)
+    node *pCur = S->pTop;
+    for (int i = 0; pCur != NULL; i++)
     {
         printf("node No. %d data is = %d\n", i, pCur->data);
         pCur = pCur->pNext;
     }
     printf("#############################\n");
+}
+
+int peekStack(Stack *S)
+{
+    int temp = pop(S);
+    push(temp, S);
+    return temp;
+}
+bool empty(Stack *S)
+{
+    return S->pTop == NULL;
+}
+
+void sortStack(Stack *S)
+{
+    Stack *temp = createStack();
+    while (!empty(S))
+    {
+        int value = pop(S); // a pivot to compare values
+
+        while (value < peekStack(temp) && !empty(temp))
+        {
+            push(pop(temp), S);
+        }
+
+        push(value, temp);
+    }
+    while (!empty(temp))
+    {
+        push(pop(temp), S);
+    }
+    if (peekStack(S) == -128)
+    {
+        pop(S);
+    }
 }
--- Stack/Stack.h
+++ Stack/Stack.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct node
 {
@@ -10,13 +11,24 @@ typedef struct node
     struct node *pNext;
 } node;
 
-node *pTop = NULL;
+typedef struct Stack
+{
+    node *pTop;
+} Stack;
 
+// creates a new stack
+Stack *createStack();
+// creates a new node
 node *createNode(int);
-void push(int);
-int pop();
-void printStack();
-
-int size = 0;
+// addes to top
+void push(int, Stack *);
+// removes from top
+int pop(Stack *);
+// prints the whole stack
+void printStack(Stack *);
+// an insertion sort like function where you sort in temp in reverse order first
+void sortStack(Stack *);
+// a function that returns the first element value without poping
+int peekStack(Stack *);
 
 #endif // STACK_HEADERFILE
